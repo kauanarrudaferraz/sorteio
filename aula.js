c05b3586323f18b82394035a53c01424f4c1ef94
kauan <kauanferraz1006@gmail.com>
2023-08-07 14:02:51 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,16 @@
+// match.random gera um número aleatorio
+console.log(Math.random());
+
+//gerando um número aleatorio entre 0 e 20
+console.log(Math.random() * 20);
+//math.round arredonda para o inteiro mais proximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+//math.ceil arredonda para cima 
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredonda para baixo 
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9))
